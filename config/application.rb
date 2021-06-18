4a7edde47b32e9e99b337595095155aca7352cba
Henning Koch <henning.koch@makandra.de>
2021-06-18 17:15:20 +0200

    Switch to secrets.yml

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,8 @@ module UnpolyDemoLayers
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.read_encrypted_secrets = false
+    config.require_master_key = false
   end
 end
