7f183918c99b39ee7871b382d2aa7445ee6ce4f6
Henning Koch <henning.koch@makandra.de>
2021-06-24 09:43:51 +0200

    Loading indicator

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,57 @@ window.addEventListener('load', (event) => {
     up.util.timer(1000, () => fragment.classList.remove('new-fragment'))
   })
 })
+
+// Show a loading indicator above the nav bar if a request takes long to load
+up.compiler('loading-indicator', (element) => {
+  let timeout = undefined
+  let transitionDelay = 300
+
+  function move() {
+    let step = 0
+    let width = 33
+
+    function moveNext() {
+      step++
+      if (width < 90) width++
+      moveTo(width)
+      timeout = setTimeout(moveNext, transitionDelay + step * 30)
+    }
+    moveNext()
+  }
+
+  function moveTo(width) {
+    element.style.width = `${width}vw`
+  }
+
+  function finish() {
+    clearTimeout(timeout)
+    moveTo(100)
+  }
+
+  function show() {
+    element.classList.add('visible')
+    element.style.transition = `width ${transitionDelay}ms ease-out`
+  }
+
+  function hide() {
+    element.style.transition = `width 0 ease-out`
+    element.classList.remove('visible')
+    moveTo(0)
+  }
+
+  function onLate() {
+    show()
+    move()
+  }
+
+  function onRecover() {
+    finish()
+    setTimeout(hide, transitionDelay)
+  }
+
+  return [
+    up.on('up:request:late', onLate),
+    up.on('up:request:recover', onRecover),
+  ]
+})
