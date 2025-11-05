2cf5eaceefae740b27bab93e2cf20689f20f558c
Henning Koch <henning.koch@makandra.de>
2025-11-05 08:33:58 +0100

    New flashes

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -3,12 +3,12 @@ module FlashesHelper
   def flashes
     html = ''.html_safe
     flash.map do |type, message|
-      html << content_tag(:div, message, class: bootstrap_flash_classes(type), role: 'alert')
+      html << content_tag(:div, message, class: flash_classes(type), role: 'alert')
     end
     html
   end
 
-  def bootstrap_flash_type(type)
+  def flash_modifier_class(type)
     case type.to_s
     when 'notice'
       'success'
@@ -19,8 +19,8 @@ module FlashesHelper
     end
   end
 
-  def bootstrap_flash_classes(type)
-    "alert alert-#{bootstrap_flash_type(type)}"
+  def flash_classes(type)
+    "flash -#{flash_modifier_class(type)}"
   end
 
 end
