335b6138bb6669612ee715129cb68cb6627f4442
Henning Koch <henning.koch@makandra.de>
2026-06-12 16:05:42 +0200

    Fix concurrent-ruby / logger loading issue

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
+require 'logger' # Work around concurrent-ruby 1.3.4+ no longer requiring logger (needed by Rails 6.1).
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
